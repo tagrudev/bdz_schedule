dfc0ab6370de7ff6d28b06790fe146caf23ca24b
Todor Grudev <tagrudev@gmail.com>
2013-01-15 16:37:13 +0200

    Sinatra - take me to the moon :)

---
+++ app.rb
@@ -0,0 +1,13 @@
+require 'sinatra'
+require 'bdz'
+require 'json'
+
+get '/' do
+  "Created by appsbakery @ github"
+end
+
+get '/search' do
+  content_type :json
+  client = Bdz::Client.new
+  client.search({:from_station => "#{params[:from_station]}", :to_station => "#{params[:to_station]}", :date => "#{params[:date]}"}).collect! { |train| train.as_json }
+end
