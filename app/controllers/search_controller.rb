46067bd8227482c52079a815d737348745393391
Todor Grudev <tagrudev@gmail.com>
2013-01-15 14:36:31 +0200

    Removing rails

--- app/controllers/search_controller.rb
+++
@@ -1,9 +0,0 @@
-class SearchController < ApplicationController
-  def index
-    m = Bdz::Client.new
-    @search = m.search({:from_station => "#{params[:from_station]}", :to_station => "#{params[:to_station]}", :date => "#{params[:date]}"})
-    respond_to do |format|
-      format.json { render :json => @search }
-    end
-  end
-end
--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Bdzschedule::Application.config.secret_token = 'c63eb8fb2266cd5a4a42b906a48de92b0c889d7693789c152330a3dded692afbf17ea625f7268a814b810ecefd6e9702cc091f3e4049acc13b9fcb6d10091962'
